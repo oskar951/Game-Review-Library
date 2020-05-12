9993a0cbdb401789bcc04d69a18ceee2ac426be1
oskar951 <ceremnovas@hotmail.com>
2020-05-12 15:40:26 +0000

    project files so far 12/05/2020

---
+++ application/routes.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from application import app
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title="Home Page")
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Page")
+
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html', title="Game Reviews")
+
+@app.route('/games')
+def games():
+    return render_template('games.html', title="Games Page")
+
+
+
