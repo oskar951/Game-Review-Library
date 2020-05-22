24f32d0d99cbf08a43efd8c80aeacbfba373b5bc
oskar951 <ceremnovas@hotmail.com>
2020-05-22 11:08:30 +0000

    test report changes

--- script/installation.sh
+++ script/installation.sh
@@ -14,6 +14,8 @@ source /var/lib/jenkins/workspace/game_freestyle/game-venv/bin/activate
 
 pip3 install -r requirements.txt
 
+pytest --cov ./application --cov-report html
+
 source ~/.bashrc
 
 gunicorn --workers=4 --bind=0.0.0.0:5000 /var/lib/jenkins/workspace/game_freestyle/application:app
