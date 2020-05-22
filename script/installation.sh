bdd3a6a8559816db55913e7d4eb9b367c75e3bb8
oskar951 <ceremnovas@hotmail.com>
2020-05-22 13:07:34 +0000

    test report changes

--- script/installation.sh
+++ script/installation.sh
@@ -10,7 +10,7 @@ sudo apt install python3-venv -y
 
 python3 -m venv game-venv
 
-source /var/lib/jenkins/workspace/game_freestyle/game-venv/bin/activate
+source game-venv/bin/activate
 
 pip3 install -r /var/lib/jenkins/workspace/game_freestyle/requirements.txt
 
