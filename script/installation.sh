6834684bb8fe3a95459d5e6de43f70455eede97a
oskar951 <ceremnovas@hotmail.com>
2020-05-22 11:26:56 +0000

    test report changes

--- script/installation.sh
+++ script/installation.sh
@@ -16,4 +16,4 @@ pip3 install -r requirements.txt
 
 source ~/.bashrc
 
-gunicorn --workers=4 --bind=0.0.0.0:5000 /var/lib/jenkins/workspace/game_freestyle/application:app
+python3 /var/lib/jenkins/workspace/game_freestyle/app.py
