d36b9480da86340c569732d587fecb819df24694
oskar951 <ceremnovas@hotmail.com>
2020-05-22 14:15:08 +0000

    test report changes

--- script/installation.sh
+++ script/installation.sh
@@ -10,16 +10,14 @@ sudo apt install python3-venv -y
 
 python3 -m venv game-venv
 
-source game-venv/bin/activate
+source /var/lib/jenkins/workspace/game_freestyle/game-venv/bin/activate
 
 pip3 install -r /var/lib/jenkins/workspace/game_freestyle/requirements.txt
 
-pip3 install gunicorn
+sudo apt install gunicorn
 
-pip3 install pytest pytest-cov flask-testing Werkzeug==0.16.1
-
-python3 pytest --cov ./application --cov-report html
+pytest --cov ./application --cov-report html
 
 source ~/.bashrc
 
-gunicorn --workers=4 --bind=0.0.0.0:5000 /var/lib/jenkins/workspace/game_freestyle/application:app
+python3 /var/lib/jenkins/workspace/game_freestyle/app.py
