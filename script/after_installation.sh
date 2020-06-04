ded4275f929916a2c497cd8970178fab95077053
oskar951 <ceremnovas@hotmail.com>
2020-06-04 11:01:37 +0000

    changes

--- script/after_installation.sh
+++ script/after_installation.sh
@@ -2,4 +2,10 @@
 
 source /var/lib/jenkins/workspace/game_pipeline/game-venv/bin/activate
 
+source /var/lib/jenkins/workspace/game_pipeline/exportvariables
+
+export DATABASE_URI="mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}"
+
+export SECRET_KEY=${SECRET_KEY}
+
 python3 /var/lib/jenkins/workspace/game_pipeline/app.py
